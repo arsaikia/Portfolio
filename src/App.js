efe6da0a808bfff298c59b8879843dc5f96b3ee7
arsaikia <arunabh.matrix@gmail.com>
2020-07-13 00:38:42 -0500

    netlify config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from 'react';
-import GlobalFonts from './fonts/fonts';
 import './index.css';
 import './App.css';
 import LandingPage from './Components/LandingPage/LandingPage.js';
@@ -7,7 +6,6 @@ import LandingPage from './Components/LandingPage/LandingPage.js';
 function App () {
 	return (
 		<Fragment>
-			<GlobalFonts />
 			<LandingPage />
 		</Fragment>
 	);
