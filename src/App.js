6ffb12942ec3467418e46e7ceb70bb00a818e8fa
arsaikia <arunabh.matrix@gmail.com>
2020-07-14 01:07:34 -0500

    Loader Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import './index.css';
 import './App.css';
+import Loader from '../src/Components/Loader/Loader';
 import LandingPage from './Components/LandingPage/LandingPage.js';
 
 function App () {
+	const [ isLoading, setIsLoading ] = useState(true);
+	useEffect(
+		() => {
+			setTimeout(() => {
+				setIsLoading(false);
+			}, 3500);
+		},
+		[ isLoading ]
+	);
+
+	if (isLoading) {
+		return <Loader />;
+	}
+
 	return (
 		<Fragment>
 			<LandingPage />
