ab482de841d85c45f188cea37a88be48f32f1b25
arsaikia <arunabh.matrix@gmail.com>
2020-07-15 01:02:36 -0500

    Updates

---
+++ precache-manifest.072f03da610933409221952e6c740def.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f37e1c583706f1a5b1e6e05dff521ee3",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "07b671d9d558b3223cbc",
+    "url": "/Portfolio/static/css/main.6b863780.chunk.css"
+  },
+  {
+    "revision": "0230928ca7dedfc19317",
+    "url": "/Portfolio/static/js/2.512cb7f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.512cb7f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07b671d9d558b3223cbc",
+    "url": "/Portfolio/static/js/main.2d681719.chunk.js"
+  },
+  {
+    "revision": "c66aa5eafc120060b1dc",
+    "url": "/Portfolio/static/js/runtime-main.efb1fe8d.js"
+  }
+]);
